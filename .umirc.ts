40b5ef5390ce119598fae20cbdd6d11fe44a1bb6
Beeant <huangxb0512@gmail.com>
2022-11-25 10:59:55 +0000

    build: docker

--- .umirc.ts
+++ .umirc.ts
@@ -7,6 +7,7 @@ export default defineConfig({
   logo: 'https://avatars0.githubusercontent.com/u/9441414?s=200&v=4',
   outputPath: '.doc',
   exportStatic: {},
+  publicPath: '/mirror/ant-compatible/',
   styles: [
     // Remove when antd fix this
     `
